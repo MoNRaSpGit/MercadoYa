5f59e92a8bb2067072ceaa858b23a038f368e32c
MoNRaSpGit <ju4nrsuarez@gmail.com>
2024-12-29 18:01:56 -0300

    home2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Componentes/Home';
 import ProductGrid from './Componentes/ProductGrid';
+import Cart from './Componentes/Cart';
 
 const App = () => {
   console.log('App montada');
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<ProductGrid />} />
-        <Route path="*" element={<div>404 - Página no encontrada</div>} />
-      </Routes>
+    <Router basename="/MercadoYa">
+      <div>
+        <Routes>
+          {/* Página de Inicio */}
+          <Route path="/" element={<Home />} />
+
+          {/* Listado de Productos */}
+          <Route path="/products" element={<ProductGrid />} />
+
+          {/* Carrito */}
+          <Route path="/cart" element={<Cart />} />
+
+          {/* Página 404 */}
+          <Route path="*" element={<div>404 - Página no encontrada</div>} />
+        </Routes>
+      </div>
     </Router>
   );
 };
