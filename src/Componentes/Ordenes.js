774dd187591bc3dcc3a9f225435de7ebfcedf494
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-01-07 19:35:43 -0300

    sdf

--- src/Componentes/Ordenes.js
+++ src/Componentes/Ordenes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, useEffect} from 'react-redux';
 import { actualizarEstadoPedidoAsync } from '../Slice/pedidoSlice';
 
 const Ordenes = () => {
